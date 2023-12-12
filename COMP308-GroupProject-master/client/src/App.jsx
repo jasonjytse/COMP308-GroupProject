31e4e5d67b0883d5635a00286f1fbdb569e32a74
unknown <eabdulk1@centennialcollege.ca>
2023-12-12 01:15:29 -0500

    components updated

--- COMP308-GroupProject-master/client/src/App.jsx
+++ COMP308-GroupProject-master/client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/LoginForm';
-import RegistrationForm from './components/RegistrationForm';
+import Login from './components/Login';
+import Registration from './components/Registration';
 
 function App() {
   return (
@@ -10,8 +10,6 @@ function App() {
       <div className="App">
         {/* Navigation Header */}
         {/* You can add a navigation header here if needed */}
-        
-        {/* Routing */}
         <Switch>
           {/* Home Page */}
           <Route exact path="/" component={Home} />
@@ -20,7 +18,7 @@ function App() {
           <Route path="/login" component={Login} />
 
           {/* Registration Page */}
-          <Route path="/register" component={RegistrationForm} />
+          <Route path="/register" component={Registration} />
           
           {/* Add more routes as needed */}
           
@@ -33,34 +31,3 @@ function App() {
 }
 
 export default App;
-
-
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
--- COMP308-GroupProject-master/client/src/components/Home.jsx
+++ COMP308-GroupProject-master/client/src/components/Home.jsx
@@ -1,57 +1,77 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
-import { Link } from 'react-router-dom';
-import './Home.css'; 
+// this is the home page component
+function Home(props)
+{
 
+    return (
+        <div>
+            <h2> Express - React with CRUD Operations</h2>
+            <p>React front-end calls Express REST API to add, 
+            list, update, or delete a user, create an course, etc.</p>
+        </div>
+    );
 
-function Home() {
-  return (
-    <Container className="home-container" maxWidth="md">
-      <header>
-        <Typography variant="h3" align="center" gutterBottom>
-          Welcome to Health Monitor
-        </Typography>
-      </header>
-      <main>
-        <section className="login-register-section">
-          <Typography variant="h4" align="center" gutterBottom>
-            Login or Register
-          </Typography>
-          <Typography variant="body1" align="center" paragraph>
-            Choose your role to get started:
-          </Typography>
-          <div className="buttons-container">
-            <Button
-              component={Link}
-              to="/login"
-              variant="contained"
-              color="primary"
-              size="large"
-              style={{ marginRight: '16px' }}
-              className="login-button" // Apply CSS class for the button
-            >
-              Login
-            </Button>
-            <Button
-              component={Link}
-              to="/register"
-              variant="contained"
-              color="secondary"
-              size="large"
-              className="register-button" // Apply CSS class for the button
-            >
-              Register
-            </Button>
-          </div>
-        </section>
-      </main>
-      <footer>
-        <Typography variant="body2" align="center" color="textSecondary">
-          &copy; 2023 Health Monitor
-        </Typography>
-      </footer>
-    </Container>
-  );
 }
-
+// withRouter will pass updated match, location, and history props 
+// to the wrapped component whenever it renders.
 export default Home;
+
+
+// import React from 'react';
+
+// import { Link } from 'react-router-dom';
+// import './Home.css'; 
+
+
+// function Home() {
+//   return (
+//     <Container className="home-container" maxWidth="md">
+//       <header>
+//         <Typography variant="h3" align="center" gutterBottom>
+//           Welcome to Health Monitor
+//         </Typography>
+//       </header>
+//       <main>
+//         <section className="login-register-section">
+//           <Typography variant="h4" align="center" gutterBottom>
+//             Login or Register
+//           </Typography>
+//           <Typography variant="body1" align="center" paragraph>
+//             Choose your role to get started:
+//           </Typography>
+//           <div className="buttons-container">
+//             <Button
+//               component={Link}
+//               to="/login"
+//               variant="contained"
+//               color="primary"
+//               size="large"
+//               style={{ marginRight: '16px' }}
+//               className="login-button" // Apply CSS class for the button
+//             >
+//               Login
+//             </Button>
+//             <Button
+//               component={Link}
+//               to="/register"
+//               variant="contained"
+//               color="secondary"
+//               size="large"
+//               className="register-button" // Apply CSS class for the button
+//             >
+//               Register
+//             </Button>
+//           </div>
+//         </section>
+//       </main>
+//       <footer>
+//         <Typography variant="body2" align="center" color="textSecondary">
+//           &copy; 2023 Health Monitor
+//         </Typography>
+//       </footer>
+//     </Container>
+//   );
+// }
+
+// export default Home;
