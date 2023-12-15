6560be567073df56cf0934cef737b94f7c79e7f5
Jason Tse <59128576+jasonjytse@users.noreply.github.com>
2023-12-15 00:21:10 -0500

    Removed duplicate bcrypt declaration

--- COMP308-GroupProject-master/server/resolvers/patient.server.resolver.js
+++ COMP308-GroupProject-master/server/resolvers/patient.server.resolver.js
@@ -5,7 +5,6 @@
 const Patient = require('../models/patient.js');
 const bcrypt = require('bcrypt');
 const config = require('../config/config');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = config.secretKey;
 const JWT_EXPIRY = config.jwtExpirySeconds;
